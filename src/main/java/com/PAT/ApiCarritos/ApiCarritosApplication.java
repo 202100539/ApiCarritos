e95e82e7a4f7b7661774d0ee28122fe84ef1851d
202100539 <202100539@alu.comillas.edu>
2026-02-25 18:54:53 +0100

    Subir comienzo codigo, proyecto vacio

---
+++ src/main/java/com/PAT/ApiCarritos/ApiCarritosApplication.java
@@ -0,0 +1,13 @@
+package com.PAT.ApiCarritos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarritosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarritosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PAT/ApiCarritos/ApiCarritosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PAT.ApiCarritos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCarritosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
